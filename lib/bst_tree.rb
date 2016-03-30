6042f11967502f7fb595dcdedcd21c138bc48b79
saylerb <sayler.b@gmail.com>
2016-03-29 23:33:52 -0600

    added ability to insert root node

--- lib/bst_tree.rb
+++ lib/bst_tree.rb
@@ -1,31 +1,33 @@
 require_relative "bst_node"
 
-class BinaryTree
+class BinarySearchTree
 
-  attr_reader :root
+  attr_accessor :root
 
-  def initialize(root)
-    @root = root 
+  def initialize
+    @root = nil  
   end
 
   # TODO: break into insert left or right
   def insert(score, title, current=@root)
-   
-    if score > current.score
-      if current.right == nil
-        current.right = Node.new(score, title)
-      else
-        insert(score, title, current.right)
+    if current == nil
+      @root = Node.new(score, title)
+    else
+      if score > current.score
+        if current.right == nil
+          current.right = Node.new(score, title)
+        else
+          insert(score, title, current.right)
+        end
+      
+      elsif score < current.score
+        if current.left == nil
+          current.left = Node.new(score,title)
+        else
+          insert(score, title, current.left)
+        end
       end
-
-    elsif score < current.score
-      if current.left == nil
-        current.left = Node.new(score,title)
-     else
-        insert(score, title, current.left)
-     end
     end
   end
-
 end
 
--- test/bst_tree_test.rb
+++ test/bst_tree_test.rb
@@ -2,22 +2,29 @@ require "minitest/autorun"
 require "minitest/pride"
 require_relative  "../lib/bst_tree"
 
+require "pry"
 class BinarySearchTreeTest < Minitest::Test
   
   def test_that_class_tree_exist
-    tree = BinaryTree.new
-    assert_equal tree.class, BinaryTree
+    tree = BinarySearchTree.new
+    assert_equal tree.class, BinarySearchTree
   end
 
-  def test_that_tree_has_root
-    root = Node.new(61, "Concussion")
-    tree = BinaryTree.new(root)
+  def test_setting_root
+    tree = BinarySearchTree.new
+    tree.root = Node.new(61, "Concussion")
+    assert_equal 61 , tree.root.score
+    assert_equal "Concussion", tree.root.title 
+
+  end
+
+  def test_that_you_can_insert_a_node
+    # binding.pry
+    tree = BinarySearchTree.new
+    tree.insert(61, "Concussion")
     assert_equal 61, tree.root.score
     assert_equal "Concussion", tree.root.title
-  end
-  
-  def test_that_you_can_insert_a_node
-  end
+  end 
 
 end
 
